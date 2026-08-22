d222fcc4d0493a3da1522ef8bc105faee5f655ed
Vince Soriano <vince@Vinces-MacBook-Air.local>
2026-08-22 12:46:39 +0800

    first commit

---
+++ individual-soriano/src/com/joysistvi/stage1/day2/SampleCode.java
@@ -0,0 +1,22 @@
+package com.joysistvi.stage1.day2;
+
+
+public class SampleCode {
+    public static void main(String[] args){
+        System.out.println("Hello Java Programming!");
+    }
+}
+
+
+/*
+    Class -> is where we write and organize related code
+          -> is a blueprint where objects are created
+*/
+
+/*
+    Class -> is where we write and organize related code
+          -> is a blueprint where objects are created
+    Syntax -> structure of code
+    Main Method (psvm) -> starting of point of programs execution
+    Print Statement (sout) -> tells the computer to print something
+*/
---
+++ individual-soriano/src/com/joysistvi/stage1/day3/Activity_Soriano.java
@@ -0,0 +1,26 @@
+package com.joysistvi.stage1.day3;
+
+public class Activity_Soriano {
+    public static void main(String[] args) {
+        String name = "Vince Soriano";
+        byte age = 20;
+        short bday = 11;
+        int favoriteNumber = 3;
+        long assets = 100000L;
+        float height = 5.5f;
+        double allowance = 1000.50;
+        char gender = 'M';
+        boolean student = false;
+
+
+        System.out.println("String - Name: " + name );
+        System.out.println("Byte - Age: " + age );
+        System.out.println("Short - BirthDay: " + bday );
+        System.out.println("Int - Favorite Number: " + favoriteNumber );
+        System.out.println("Long - Assets: " + assets );
+        System.out.println("Float - Height: " + height );
+        System.out.println("Double - Allowance: " + allowance );
+        System.out.println("Char - Gender: " + gender );
+        System.out.println("Boolean - are you a student? " + student);
+    }
+}
